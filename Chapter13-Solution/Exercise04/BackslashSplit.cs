645a0baafcf5cde727b93e9bba467ca88a293f95
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-13 15:03:12 +0100

    Edit ex04

--- Chapter13-Solution/Exercise04/BackslashSplit.cs
+++ Chapter13-Solution/Exercise04/BackslashSplit.cs
@@ -3,14 +3,14 @@
 namespace Exercise04
 {
     /// <summary>
-    /// 4. How many backslashes you must specify as an argument to the method Split(…) in order to split the text by a backslash? Ans: 1
+    /// 4. How many backslashes you must specify as an argument to the method Split(…) in order to split the text by a backslash? Ans: 2
     /// </summary>
     internal class BackslashSplit
     {
         private static void Main(string[] args)
         {
-            string str = "one/two/three/four/five//six//seven////eight";
-            string[] newString = str.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            string str = @"one\two\three\four\five\\six\\seven\\\\eight";
+            string[] newString = str.Split('\\', StringSplitOptions.RemoveEmptyEntries);
             foreach (string s in newString)
             {
                 Console.WriteLine(s);

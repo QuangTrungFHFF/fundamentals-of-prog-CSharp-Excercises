ad66a6f5e287b9f6db2547c603e474ac6e4b0c19
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-13 15:44:32 +0100

    Chapter 13 add Exercise 8

---
+++ Chapter13-Solution/Exercise08/CsharpUnicodeLiterals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise08
+{
+    /// <summary>
+    /// Write a program that converts a given string into the form of array of Unicode escape sequences in the format used in the C# language. 
+    /// Sample input: "Test". Result: "\u0054\u0065\u0073\u0074".
+    /// </summary>
+    internal class CsharpUnicodeLiterals
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string");
+            string str = Console.ReadLine();
+
+            var result = str.Select(t => $"\\u{Convert.ToInt16(t):X4}").ToList();
+
+            foreach (string s in result)
+            {
+                Console.Write(s);
+            }
+        }
+    }
+}

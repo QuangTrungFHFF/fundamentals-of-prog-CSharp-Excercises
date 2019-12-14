a5b23c1569f9575bb6005db229232a392167689c
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-14 13:53:18 +0100

    Chapter 13 add Excercise 11

---
+++ Chapter13-Solution/Exercise11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise11
+{
+    /// <summary>
+    /// 11. A string is given, composed of several "forbidden" words separated by commas. Also a text is given, containing those words.
+    /// Write a program that replaces the forbidden words with asterisks.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Microsoft announced its next generation C# compiler today. It uses advanced parser and special optimizer for the Microsoft CLR.";
+            List<string> forbiddenTexts = new List<string> { "C#","CLR","Microsoft","wtf" };
+            
+            for(int i=0; i<forbiddenTexts.Count;i++)
+            {
+                text = text.Replace(forbiddenTexts[i], GetReplaceString(forbiddenTexts[i]));
+            }
+            Console.WriteLine(text);
+            Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Replace string with "*" using StringBuilder 
+        /// Can also use: string replaced = new string('*', inputStr.Length);
+        /// </summary>
+        /// <param name="inputStr"></param>
+        /// <returns></returns>
+        private static string GetReplaceString(string inputStr)
+        {
+            StringBuilder replaced = new StringBuilder();
+            for(int i =0; i< inputStr.Length;i++)
+            {
+                replaced.Append("*");
+            }
+            return replaced.ToString();
+        }
+    }
+}

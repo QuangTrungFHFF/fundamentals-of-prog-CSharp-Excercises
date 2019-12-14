238f622f782ff735540c01d27d8e8d5d8df6c2f8
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-14 18:54:25 +0100

    Chapter 13 add Exercise 14

---
+++ Chapter13-Solution/Exercise14/ReverseSentence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise14
+{
+    /// <summary>
+    /// Write a program that reverses the words in a given sentence without changing punctuation and spaces.
+    /// For example: "C# is not C++ and PHP is not Delphi"  "Delphi not is PHP and C++ not is C#".
+    /// </summary>
+    internal class ReverseSentence
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence!");
+            string sentence = Console.ReadLine();
+            string[] reverse = sentence.Split(' ');
+            Console.WriteLine("Press any key to reverse the sentence!");
+            Console.ReadKey(false);
+            for (int i = reverse.Length - 1; i >= 0; i--)
+            {
+                Console.Write(reverse[i] + " ");
+            }
+        }
+    }
+}

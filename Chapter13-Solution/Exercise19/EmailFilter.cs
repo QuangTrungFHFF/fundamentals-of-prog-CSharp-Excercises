e51d8a5613a3b519c48ed980afa4722e237a42e0
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-15 12:53:06 +0100

    Chapter 13 add Exercise 19

---
+++ Chapter13-Solution/Exercise19/EmailFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise19
+{
+    /// <summary>
+    /// Write a program that extracts all e-mail addresses from a text. 
+    /// These are all substrings that are limited on both sides by text end or separator between words and match the shape <sender>@<host>…<domain>.
+    /// </summary>
+    internal class EmailFilter
+    {
+        private static void Main(string[] args)
+        {
+            string text = "Please contact us by phone (+001 222 222 222) or by email at example@gmail.com or at test.user@yahoo.co.uk . This is not email: test@test. This also: @gmail.com. Neither this: a@a.b.";
+            string[] splitedText = text.Split(' ');
+            CheckEmail(splitedText);
+            Console.WriteLine("End!");
+        }
+
+        private static void CheckEmail(string[] str)
+        {
+            Regex filter = new Regex(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (filter.IsMatch(str[i]))
+                {
+                    Console.WriteLine(str[i]);
+                }
+            }
+        }
+    }
+}

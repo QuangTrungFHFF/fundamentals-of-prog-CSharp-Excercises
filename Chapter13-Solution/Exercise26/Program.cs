6ba7b70d1712a1c2bb5039929a6a7aab35eceeda
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-15 16:43:37 +0100

    Chapter 13 add Exercise 26

---
+++ Chapter13-Solution/Exercise26/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise26
+{
+    /// <summary>
+    /// Write a program that extracts all the text without any tags and attribute values from an HTML document.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = String.Empty;
+            string text = "<html>< head >< title > News </ title ></ head >< body >< p >< a href = \"http://softuni.org\" > Software University </ a > aims to provide free real-world practical"
+                + "training for young people who want to turn into skillful software engineers.</ p ></ body ></ html > ";
+            
+            Regex regex = new Regex(@">(?<word>.*?)<"); 
+            MatchCollection word = regex.Matches(text);
+            foreach(Match m in word)
+            {
+                if((result = m.Groups[1].Value)!="")
+                {
+                    Console.WriteLine(result);
+                }
+                
+            }
+
+
+
+
+        }
+    }
+}

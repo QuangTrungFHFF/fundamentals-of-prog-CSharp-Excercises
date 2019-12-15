86dc7ced5ce2ca0969357cfb260d9c272b3c8e87
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-15 15:47:24 +0100

    Chapter 13 add Exercise 24

---
+++ Chapter13-Solution/Exercise24/RepeatingLetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Exercise24
+{
+    /// <summary>
+    /// Write a program that reads a string from the console and replaces every sequence of identical letters in it with a single letter (the repeating letter).
+    /// Example: "aaaaabbbbbcdddeeeedssaa"  "abcdedsa".
+    /// </summary>
+    internal class RepeatingLetters
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string!");
+            string text = Console.ReadLine();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(text[0]);
+            for(int i=1;i<text.Length;i++)
+            {
+                if(!text[i].Equals(text[i-1]))
+                {
+                    stringBuilder.Append(text[i]);
+                }
+            }
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}

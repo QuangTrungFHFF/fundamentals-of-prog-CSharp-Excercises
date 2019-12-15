78f1f25c7bb8411b6f8177d17ba06804971d7e3d
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-15 11:56:34 +0100

    Chapter 13 add Exercise 18

---
+++ Chapter13-Solution/Exercise18/After630.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise18
+{
+    /// <summary>
+    /// Write a program that reads the date and time entered in the format "day.month.year hour:minutes:seconds" and prints the date and time after 6 hours and 30 minutes in the same format.
+    /// </summary>
+    internal class After630
+    {
+        private static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+            TimeSpan interval = new TimeSpan(6, 30, 0);
+            Console.WriteLine("Please enter a date and time!");
+            if (DateTime.TryParse(Console.ReadLine(), out dateTime))
+            {
+                Console.WriteLine("After {0} hours {1} minutes, the new time is: {2}", interval.Hours, interval.Minutes, dateTime + interval);
+            }
+            else
+            {
+                Console.WriteLine("Invalid format. Try again with dd.mm.yyyy hh:mm:ff.");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}

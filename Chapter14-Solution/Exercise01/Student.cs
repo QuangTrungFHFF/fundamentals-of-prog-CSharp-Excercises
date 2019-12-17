7e27d23a5fd69c1dc3d68d48cd71f16b11c00b60
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-17 19:04:45 +0100

    Chapter 14 add Exercise 1

---
+++ Chapter14-Solution/Exercise01/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise01
+{
+    /// <summary>
+    /// Define a class Student, which contains the following information about students: full name, course, subject, university, e-mail and phone number.
+    /// </summary>
+    internal class Student
+    {
+        private string fullName;
+        private string course;
+        private string subject;
+        private string university;
+        private string eMail;
+        private string phoneNumber;
+
+        public Student()
+        {
+        }
+    }
+}

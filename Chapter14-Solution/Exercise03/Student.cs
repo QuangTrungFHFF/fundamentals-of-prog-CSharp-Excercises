42cb5ce8210c433b4e68c88ccd125ee1b6a65402
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-17 19:35:09 +0100

    Chapter 14 add Exercise 04

---
+++ Chapter14-Solution/Exercise03/Student.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercise03
+{
+    /// <summary>
+    /// Add a static field for the class Student, which holds the number of created objects of this class.
+    /// </summary>
+    internal class Student
+    {
+        private static int numberOfStudent;
+        private string fullName;
+        private int course;
+        private string subject;
+        private string university;
+        private string eMail;
+        private string phoneNumber;
+
+        public Student(string fullName, int course, string subject, string university, string eMail, string phoneNumber)
+        {
+            this.fullName = fullName;
+            this.course = course;
+            this.subject = subject;
+            this.university = university;
+            this.eMail = eMail;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public Student(string fullName, int course, string subject, string university)
+            : this(fullName, course, subject, university, null, null)
+        {
+        }
+
+        public Student(string fullName, string university) : this(fullName, 0, null, university, null, null)
+        {
+        }
+    }
+}
---
+++ Chapter14-Solution/Exercise04/Student.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace Exercise04
+{
+    /// <summary>
+    /// Add a method in the class Student, which displays complete information about the student.
+    /// </summary>
+    internal class Student
+    {
+        private static int numberOfStudent;
+        private string fullName;
+        private int course;
+        private string subject;
+        private string university;
+        private string eMail;
+        private string phoneNumber;
+
+        public Student(string fullName, int course, string subject, string university, string eMail, string phoneNumber)
+        {
+            this.fullName = fullName;
+            this.course = course;
+            this.subject = subject;
+            this.university = university;
+            this.eMail = eMail;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public Student(string fullName, int course, string subject, string university)
+            : this(fullName, course, subject, university, null, null)
+        {
+        }
+
+        public Student(string fullName, string university) : this(fullName, 0, null, university, null, null)
+        {
+        }
+
+        /// <summary>
+        /// Display info about the student
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder info = new StringBuilder();
+            info.Append("Student name: " + fullName);
+            info.Append(System.Environment.NewLine);
+            info.Append("Course: " + course);
+            info.Append(System.Environment.NewLine);
+            info.Append("Subject: " + subject);
+            info.Append(System.Environment.NewLine);
+            info.Append("University: " + university);
+            info.Append(System.Environment.NewLine);
+            info.Append("E-Mail: " + eMail);
+            info.Append(System.Environment.NewLine);
+            info.Append("Telephone: " + phoneNumber);
+            return info.ToString();
+        }
+    }
+}

f750e271c3d96a6d988d727e027acc10c66175ba
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-18 21:33:18 +0100

    Chapter 14 add Exercise 7

---
+++ Chapter14-Solution/Exercise07/Student.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace Exercise07
+{
+    /// <summary>
+    /// Modify the current source code of Student class so as to encapsulate the data in the class using properties.
+    /// </summary>
+    internal class Student
+    {
+        static int numberOfStudent = 0;
+
+        public Student(string fullName, int course, string subject, string university, string email, string phoneNumber)
+        {
+            this.FullName = fullName;
+            this.Course = course;
+            this.Subject = subject;
+            this.University = university;
+            this.Email = email;
+            this.PhoneNumber = phoneNumber;
+            numberOfStudent++;
+        }
+
+        public Student(string fullName, int course, string subject, string university)
+            : this(fullName, course, subject, university, null, null)
+        {
+        }
+
+        public Student(string fullName, string university) : this(fullName, 0, null, university, null, null)
+        {
+        }
+
+        /// <summary>
+        /// Display info about the student
+        /// </summary>
+        /// <returns>String contains info about students</returns>
+        public override string ToString()
+        {
+            StringBuilder info = new StringBuilder();
+            info.Append("Student name: " + FullName);
+            info.Append(System.Environment.NewLine);
+            info.Append("Course: " + Course);
+            info.Append(System.Environment.NewLine);
+            info.Append("Subject: " + Subject);
+            info.Append(System.Environment.NewLine);
+            info.Append("University: " + University);
+            info.Append(System.Environment.NewLine);
+            info.Append("E-Mail: " + Email);
+            info.Append(System.Environment.NewLine);
+            info.Append("Telephone: " + PhoneNumber);            
+            info.Append(System.Environment.NewLine);
+            info.Append("-------------------------------");
+            return info.ToString();
+        }
+
+        public static int GetNumberOfStudents
+        {
+            get { return Student.numberOfStudent; }
+            set { numberOfStudent = value; }
+        }
+
+        public string FullName { get; set; }
+        public int Course { get; set; }
+        public string Subject { get; set; }
+        public string University { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Chapter14-Solution/Exercise07/StudentTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Write a class StudentTest, which has to test the functionality of the class Student.
+/// </summary>
+namespace Exercise07
+{
+    internal class StudentTest
+    {
+        private static List<Student> studentList = new List<Student>();
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to begin test!");
+            Console.ReadLine();
+            AddStudent();
+            Console.WriteLine("Test success!");
+            Console.WriteLine("Press any key to print student list!");
+            Console.ReadKey();
+            PrintStudentList();
+        }
+
+        /// <summary>
+        /// Add 3 new students
+        /// </summary>
+        public static void AddStudent()
+        {
+            List<string> names = new List<string>() { "Jimmy Wood", "Toby Thomas", "Maximus Martin" };
+            List<int> courses = new List<int>() { 3, 2, 1 };
+            List<string> subjects = new List<string>() { "c#", "Java", "Python" };
+            List<string> universities = new List<string>() { "Ludwig Maximilian University of Munich", "Heidelberg  University", "Humboldt University of Berlin" };
+            List<string> emails = new List<string>() { "tata521ta@xmail.com", "bibobiba@superhotmail.com", "maximaluma@yahuu.com" };
+            List<string> phoneNumbers = new List<string>() { "015728956324", "0908745214", "012745632145" };
+            for (int i = 0; i < 3; i++)
+            {
+                StudentTest.studentList.Add(new Student(names[i], courses[i], subjects[i], universities[i], emails[i], phoneNumbers[i]));
+            }
+            Console.WriteLine("3 students added!");
+        }
+
+        /// <summary>
+        /// Print result list to the screen
+        /// </summary>
+        public static void PrintStudentList()
+        {
+            Console.WriteLine("Number of students: {0}", Student.GetNumberOfStudents);
+            for (int i = 0; i < studentList.Count; i++)
+            {
+                Console.WriteLine(studentList[i].ToString());
+            }
+        }
+    }
+}

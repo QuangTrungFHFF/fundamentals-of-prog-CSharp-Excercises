68e26f20c084c698bf474378c683962c00bec96e
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-20 17:16:05 +0100

    Chapter 14 add Exercise 20

---
+++ Chapter14-Solution/Exercise20/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace Exercise20
+{
+    /// <summary>
+    /// The books must contain the title, author, publisher, release date and ISBN-number.
+    /// </summary>
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string ISBN { get; set; }
+        public Book()
+        {
+            Title = null;
+            Author = null;
+            Publisher = null;
+            ReleaseDate = null;
+            ISBN = null;
+        }
+        public Book(string title, string author, string publisher, DateTime? releaseDate, string iSBN)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            ReleaseDate = releaseDate;
+            ISBN = iSBN;
+        }
+        public Book(string title, string author) : this(title,author,null,null,null)
+        {
+        }
+        public Book(string title, string author, string publisher):this(title,author,publisher,null,null)
+        {
+        }
+        public override string ToString()
+        {
+            StringBuilder info = new StringBuilder();
+            info.Append("Title: " + Title);
+            info.Append(Environment.NewLine);
+            info.Append("Author: " + Author);
+            info.Append(Environment.NewLine);
+            info.Append("Publisher: " + Publisher);
+            info.Append(Environment.NewLine);
+            info.Append("Release date: " + ReleaseDate?.ToString("d"));
+            info.Append(Environment.NewLine);
+            info.Append("ISBN: " + ISBN);
+            info.Append(Environment.NewLine);
+            info.Append("-----------------------------------------------");
+            return info.ToString();
+        }
+    }
+}
---
+++ Chapter14-Solution/Exercise20/Library.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise20
+{
+    /// <summary>
+    /// The library must contain a name and a list of books. 
+    /// </summary>
+    class Library
+    {
+        private List<Book> bookList;
+        public string Name { get; set; }
+        public List<Book> BookList
+        {
+            get { return bookList; }
+            set { bookList = value; }
+        }
+        public Library()
+        {
+            BookList = new List<Book>();
+            Name = "Default Name";
+        }
+        public Library(string name)
+        {
+            BookList = new List<Book>();
+            Name = name;
+        }
+        public Library(string name, List<Book> bookList)
+        {
+            BookList = bookList;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Add book to the library
+        /// </summary>
+        /// <param name="book"></param>
+        public void AddBook(Book book)
+        {
+            this.BookList.Add(book);            
+        }
+        public void AddBook(string title, string author)
+        {
+            BookList.Add(new Book(title, author));
+        }
+        public void AddBook(string title, string author, string publisher, DateTime? releaseDate, string iSBN)
+        {
+            BookList.Add(new Book(title, author, publisher, releaseDate, iSBN));
+        }
+
+        /// <summary>
+        /// Delete one book on the library
+        /// </summary>
+        /// <param name="book"></param>
+        public void DeleteBook(Book book)
+        {
+            if(BookList.Contains(book))
+            {
+                BookList.Remove(book);
+            }           
+        }
+        /// <summary>
+        /// Remove all books from Library
+        /// </summary>
+        public void ClearLibrary()
+        {
+            BookList.Clear();
+        }
+        /// <summary>
+        /// Search for a book by a predefined author
+        /// </summary>
+        /// <param name="author"></param>
+        public void SearchBookByAuthor(string author)
+        {
+            foreach(Book b in BookList)
+            {
+                if(b.Author.Equals(author,StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(b.ToString());
+                }
+            }
+        }
+
+    }
+}

db879fc51c0cc1e7a5a0581367c3e2f8856d2d19
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-26 15:55:20 +0100

    Chapter 15 add Exercise 3

---
+++ Chapter15-Solution/Exercise03/AddLineNumber.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise03
+{
+    /// <summary>
+    /// Write a program that reads the contents of a text file and inserts the line numbers at the beginning of each line, then rewrites the file contents.
+    /// </summary>
+    class AddLineNumber
+    {
+        static void Main(string[] args)
+        {
+            string sourceName = "Exercise03.txt";
+            string tempName = "Exercise03temp.txt";
+            string baseFolder = Path.GetFullPath(@"..\..\..\..\") + @"Textfiles\";
+            string sourceFile = Path.Combine(baseFolder, sourceName );
+            string tempFile = Path.Combine(baseFolder,tempName);            
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(sourceFile))
+                using (StreamWriter streamWriter = new StreamWriter(tempFile))
+                {
+                    string line;
+                    int lineCount = 0;
+                    while((line = streamReader.ReadLine())!= null)
+                    {
+                        lineCount++;
+                        streamWriter.WriteLine(InsertLineNumber(line, lineCount));
+                    }
+                }
+                File.Replace(tempFile, sourceFile, null);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine("Cannot find the source file!");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.Error.WriteLine("Invalid folder path!");
+            }
+            catch(IOException ex)
+            {
+                Console.Error.WriteLine("Cannot read/write the file!" + ex.Message);
+            }
+            Console.WriteLine("Added Line number to the source file.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Insert line number at the start of the string
+        /// </summary>
+        /// <param name="content">Content of the line</param>
+        /// <param name="lineCount"></param>
+        /// <returns></returns>
+        public static string InsertLineNumber(string content,int lineCount)
+        {
+            return string.Format($"Line {lineCount}: {content}");
+        }
+    }
+}

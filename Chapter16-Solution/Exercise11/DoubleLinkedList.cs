dab25d7b2e8640c8578644aff8d571ed26f6ef2b
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-29 20:55:23 +0100

    Chapter 16 update Exercise 11

--- Chapter16-Solution/Exercise11/DoubleLinkedList.cs
+++ Chapter16-Solution/Exercise11/DoubleLinkedList.cs
@@ -8,11 +8,12 @@ namespace Exercise11
     /// and a method, which returns an array with the elements of the list.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-   class DoubleLinkedList<T> where T : IComparable
+    internal class DoubleLinkedList<T> where T : IComparable
     {
         public DoubleLinkedListNode<T> First { get; private set; }
         public DoubleLinkedListNode<T> Last { get; private set; }
         public int Count { get; private set; }
+
         public DoubleLinkedList()
         {
             First = null;
@@ -20,9 +21,13 @@ namespace Exercise11
             Count = 0;
         }
 
+        /// <summary>
+        /// Add an element at the beginning of the list
+        /// </summary>
+        /// <param name="value"></param>
         public void AddFirst(T value)
         {
-            if(First == null)
+            if (First == null)
             {
                 First = Last = new DoubleLinkedListNode<T>(value);
             }
@@ -34,9 +39,14 @@ namespace Exercise11
             }
             Count++;
         }
+
+        /// <summary>
+        /// Add an element to the end of the list
+        /// </summary>
+        /// <param name="value"></param>
         public void AddLast(T value)
         {
-            if(Last == null)
+            if (Last == null)
             {
                 First = Last = new DoubleLinkedListNode<T>(value);
             }
@@ -48,6 +58,10 @@ namespace Exercise11
             }
             Count++;
         }
+
+        /// <summary>
+        /// Remove the first element of the list
+        /// </summary>
         public void RemoveFirst()
         {
             if (First == null)
@@ -55,12 +69,16 @@ namespace Exercise11
                 Console.WriteLine("List is empty!");
             }
             else
-            {                 
+            {
                 First = First.Next;
                 First.Previous = null;
             }
             Count--;
         }
+
+        /// <summary>
+        /// Remove an element to the end of the list
+        /// </summary>
         public void RemoveLast()
         {
             if (Last == null)
@@ -68,21 +86,26 @@ namespace Exercise11
                 Console.WriteLine("List is empty!");
             }
             else
-            {                
+            {
                 Last = Last.Previous;
                 Last.Next = null;
             }
             Count--;
         }
+
+        /// <summary>
+        /// Remove the first element with the given value
+        /// </summary>
+        /// <param name="value"></param>
         public void Remove(T value)
         {
             DoubleLinkedListNode<T> currentNode = First;
             bool isRemoved = false;
-            while(currentNode!=null&&isRemoved==false)
+            while (currentNode != null && isRemoved == false)
             {
-                if(currentNode.Value.CompareTo(value) == 0)
+                if (currentNode.Value.CompareTo(value) == 0)
                 {
-                    if(currentNode.Previous ==null)
+                    if (currentNode.Previous == null)
                     {
                         this.RemoveFirst();
                     }
@@ -105,12 +128,17 @@ namespace Exercise11
                 }
             }
         }
+        /// <summary>
+        /// Find the first element with the given value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public DoubleLinkedListNode<T> Find(T value)
         {
-            DoubleLinkedListNode<T> currentNode = First;            
-            while(currentNode!=null)
+            DoubleLinkedListNode<T> currentNode = First;
+            while (currentNode != null)
             {
-                if(currentNode.Value.CompareTo(value) ==0)
+                if (currentNode.Value.CompareTo(value) == 0)
                 {
                     return currentNode;
                 }
@@ -118,12 +146,18 @@ namespace Exercise11
             }
             return null;
         }
-        public void AddBefore(T value,int index)
+
+        /// <summary>
+        /// Add an element before a given index
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="index"></param>
+        public void AddBefore(T value, int index)
         {
             DoubleLinkedListNode<T> currentNode = First;
             DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(value);
             int currentIndex = 0;
-            if(index == 0)
+            if (index == 0)
             {
                 this.AddFirst(value);
             }
@@ -131,34 +165,39 @@ namespace Exercise11
             {
                 this.AddLast(value);
             }
-            else if(index > Count|| index <0)
+            else if (index > Count || index < 0)
             {
                 Console.Error.WriteLine("Out of range");
             }
             else
             {
-                while(currentIndex != index)
+                while (currentIndex != index)
                 {
                     currentNode = currentNode.Next;
                     currentIndex++;
-                }
-                currentNode.Previous = newNode;
-                currentNode.Previous.Next = newNode;
+                }                                               
                 newNode.Previous = currentNode.Previous;
+                currentNode.Previous.Next = newNode;
+                currentNode.Previous = newNode;
                 newNode.Next = currentNode;
                 Count++;
             }
-            
         }
+
+        /// <summary>
+        /// Add an element after a given index
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="index"></param>
         public void AddAfter(T value, int index)
         {
             DoubleLinkedListNode<T> currentNode = First;
             DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(value);
             int currentIndex = 0;
-            if (index == (Count-1))
+            if (index == (Count - 1))
             {
                 this.AddLast(value);
-            }            
+            }
             else if (index >= Count || index < 0)
             {
                 Console.Error.WriteLine("Out of range");
@@ -173,11 +212,52 @@ namespace Exercise11
                 newNode.Next = currentNode.Next;
                 newNode.Previous = currentNode;
                 currentNode.Next.Previous = newNode;
-                currentNode.Next = newNode; 
+                currentNode.Next = newNode;
                 Count++;
             }
-
         }
 
+        /// <summary>
+        /// Retrieving an element by a given index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public DoubleLinkedListNode<T> GetElementAtIndex(int index)
+        {
+            DoubleLinkedListNode<T> currentNode = First;
+            int currentIndex = 0;
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                while (currentIndex != index)
+                {
+                    currentNode = currentNode.Next;
+                    currentIndex++;
+                }
+                return currentNode;
+            }
+        }
+
+        /// <summary>
+        /// Returns an array with the elements of the list
+        /// </summary>
+        /// <returns></returns>
+        public T[] ToArray()
+        {
+            DoubleLinkedListNode<T> currentNode = First;
+            T[] result = new T[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                
+                result[i] = currentNode.Value;
+                               
+
+                currentNode = currentNode.Next;
+            }
+            return result;
+        }
     }
 }
--- Chapter16-Solution/Exercise11/TestProgram.cs
+++ Chapter16-Solution/Exercise11/TestProgram.cs
@@ -2,11 +2,47 @@
 
 namespace Exercise11
 {
-    class TestProgram
+    internal class TestProgram
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test!");
+            //Create new LinkedList
+            DoubleLinkedList<int> testLinkedList = new DoubleLinkedList<int>();
+            //Add an element at beginning of the List
+            testLinkedList.AddFirst(100);
+            PrintResult(testLinkedList.ToArray());
+            //Add an element at the end of the List
+            testLinkedList.AddLast(200);
+            PrintResult(testLinkedList.ToArray());
+            testLinkedList.AddFirst(0);
+            //Add an element after/before a given index
+            testLinkedList.AddAfter(50, 0);
+            testLinkedList.AddBefore(150, 3);
+            PrintResult(testLinkedList.ToArray());
+            testLinkedList.AddAfter(250, 4);
+            testLinkedList.AddBefore(300, 6);
+            PrintResult(testLinkedList.ToArray());
+            ////Remove the first and last elements of the List
+            testLinkedList.RemoveFirst();
+            testLinkedList.RemoveLast();
+            PrintResult(testLinkedList.ToArray());
+            //Remove the first element with value equal 100
+            testLinkedList.Remove(100);
+            PrintResult(testLinkedList.ToArray());
+            testLinkedList.AddLast(testLinkedList.Find(250).Value);
+            PrintResult(testLinkedList.ToArray());
+            //Get the element at a given index
+            Console.WriteLine("Element at index 3 is: {0}", testLinkedList.GetElementAtIndex(3).Value);
+        }
+
+        public static void PrintResult(int[] array)
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n-----------------------------------");
         }
     }
 }

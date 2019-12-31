2071cfb565ecf387229b3f31b0242c6491cf592d
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-31 14:36:08 +0100

    Chapter 16 add Exercise 15

---
+++ Chapter16-Solution/Exercise15/BubbleSortLinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise15
+{
+    public static class BubbleSortLinkedList
+    {
+        // This is the extension method.
+        // The first parameter takes the "this" modifier
+        // and specifies the type for which the method is defined.
+        public static LinkedList<double> BubbleSort(this LinkedList<double> linkedList)
+        {
+            if(linkedList == null||linkedList.Count ==1)
+            {
+                Console.WriteLine("Nothing to sort!");                
+            }
+            else
+            {
+                //Scan from beginning of the list, if current node > next node then swap them. 
+                //Loop the scan process until the loop without any swap
+                bool isSwapFree = false;
+                while(!isSwapFree)
+                {
+                    isSwapFree = true;
+                    for(LinkedListNode<double> currentNode = linkedList.First;currentNode.Next!=null;currentNode = currentNode.Next)
+                    {
+                        if(currentNode.Value>currentNode.Next.Value)
+                        {
+                            double tempValue = currentNode.Value;
+                            currentNode.Value = currentNode.Next.Value;
+                            currentNode.Next.Value = tempValue;
+                            isSwapFree = false;
+                        }
+                    }
+                }                
+            }
+            return linkedList;
+        }
+    }
+}
---
+++ Chapter16-Solution/Exercise15/Testprogram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise15
+{
+    /// <summary>
+    /// Implement numbers sorting in a dynamic linked list without using an additional array or other data structure.
+    /// </summary>
+    class Testprogram
+    {
+        static void Main(string[] args)
+        {
+            double[] data = { 0.52, 1.635, 5, 4, 8, 9, 6, 3, 145, 15.25, 5.36 };
+            var doubleList = new LinkedList<double>(data);
+            doubleList.BubbleSort();
+            foreach(double d in doubleList)
+            {
+                Console.Write(d + " ");
+            }
+            
+        }        
+        
+    }
+}

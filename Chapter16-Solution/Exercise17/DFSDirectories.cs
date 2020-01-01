65d5fda2d1a06c4a6829e74ac9c72ac1b75e9469
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-01 21:28:01 +0100

    Chapter 16 add Exercise 17

---
+++ Chapter16-Solution/Exercise17/DFSDirectories.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exercise17
+{
+    /// <summary>
+    /// Using stack, implement a complete traversal of all directories on your hard disk and print them on the console.
+    /// Implement the algorithm Depth-First-Search (DFS) – you may find some articles in the internet.
+    /// </summary>
+    internal class DFSDirectories
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Print all sub-directories using DFS!");
+            string currentDirectory = @"D:\";
+            var directories = new Stack<string>();
+            string[] subDirectories;
+            directories.Push(currentDirectory);
+            while (directories.Count > 0)
+            {
+                currentDirectory = directories.Pop();
+                Console.WriteLine(currentDirectory);
+                try
+                {
+                    subDirectories = Directory.GetDirectories(currentDirectory);
+                    foreach (string s in subDirectories)
+                    {
+                        directories.Push(s);
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}

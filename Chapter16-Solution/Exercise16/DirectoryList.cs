a6035befc5e42da07b993cdb12f22780a9476635
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-01 20:48:18 +0100

    Chapter 16 add Exercise 16

---
+++ Chapter16-Solution/Exercise16/DirectoryList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exercise16
+{
+    /// <summary>
+    /// Using queue, implement a complete traversal of all directories on your hard disk and print them on the console.
+    /// Implement the algorithm Breadth-First-Search (BFS) – you may find some articles in the internet.
+    /// </summary>
+    internal class DirectoryList
+    {
+        private static void Main(string[] args)
+        {
+            Queue<string> directories = new Queue<string>();
+            Console.WriteLine("Print all sub directory!");
+            string[] dir;
+            string currentdir = @"D:\";
+            directories.Enqueue(currentdir);
+            while (directories.Count > 0)
+            {
+                currentdir = directories.Dequeue();
+                Console.WriteLine(currentdir);
+                try
+                {
+                    dir = Directory.GetDirectories(currentdir);
+                    foreach (string s in dir)
+                    {
+                        directories.Enqueue(s);
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}

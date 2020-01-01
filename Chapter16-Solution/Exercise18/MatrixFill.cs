4a399cf03c82a91146b323ef289245ca5ccbb6d9
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-01 23:10:38 +0100

    Chapter 16 add Exercise 18

---
+++ Chapter16-Solution/Exercise18/MatrixFill.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise18
+{
+    /// <summary>
+    /// We are given a labyrinth of size N x N. Some of the cells of the labyrinth are empty (0), and others are filled (x).
+    /// We can move from an empty cell to another empty cell, if the cells are separated by a single wall.
+    /// We are given a start position (*). Calculate and fill the labyrinth as follows: in each empty cell put the minimal distance from the start position to this cell.
+    /// If some cell cannot be reached, fill it with "u".
+    /// </summary>
+    class MatrixFill
+    {
+        static void Main(string[] args)
+        {
+            string[,] matrix = new string[,] { {"0","0","0","x","0","x"},
+                                               {"0","x","0","x","0","x"},
+                                               {"0","a","x","0","x","0"},
+                                               {"0","x","0","0","0","0"},
+                                               {"0","0","0","x","x","0"},
+                                               {"0","0","0","x","0","x"} };            
+            PathFinding markedMatrix = new PathFinding(matrix);            
+            string[,] newMatrix = markedMatrix.MarkMatrix(); 
+            for(int i =0; i< newMatrix.GetLength(0);i++)
+            {
+                for (int j = 0; j< newMatrix.GetLength(1);j++)
+                {
+                    Console.Write(newMatrix[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Chapter16-Solution/Exercise18/PathFinding.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise18
+{
+    class PathFinding
+    {
+        public HashSet<Point> markedPoints = new HashSet<Point>();
+        public HashSet<Point> MarkedPoint
+        {
+            get { return markedPoints; }
+        }  
+        public string[,] Matrix { get; set; }
+        public Point StartPoint { get; set; }
+        public PathFinding(string[,] matrix)
+        {
+            Matrix = matrix;
+        }
+        public string[,] MarkMatrix()
+        {
+            string[,] matrix = Matrix;
+            Queue<Point> paths = new Queue<Point>();
+            int count = 0;
+            FindStartPoint();
+            Point currentPoint = StartPoint;
+            List<Point> neighbors;
+            paths.Enqueue(StartPoint);
+            Console.WriteLine(StartPoint.X);
+            Console.WriteLine(StartPoint.Y);
+            markedPoints.Add(new Point(StartPoint.X, StartPoint.Y));
+            
+            while(paths.Count>0)
+            {
+                currentPoint = paths.Dequeue();
+                matrix[currentPoint.X, currentPoint.Y] = "e";
+                Console.WriteLine(Matrix[currentPoint.X, currentPoint.Y]);
+                neighbors = GetPassableNeighbors(currentPoint);
+                foreach(Point p in neighbors)
+                {
+                    paths.Enqueue(p);
+                    markedPoints.Add(p); 
+                }
+            }
+            return matrix;
+
+
+        }
+        public void FindStartPoint()
+        {
+             
+            for(int row = 0; row < Matrix.GetLength(0);row++)
+            {
+                for(int column = 0; column<Matrix.GetLength(1);column++)
+                {
+                    if(Matrix[row,column].Equals("a"))
+                    {
+                        StartPoint = new Point(row, column);                        
+                    }
+                }
+            }
+            
+            
+        }
+        public List<Point> GetPassableNeighbors(Point currentPoint)
+        {
+            var passablePoints = new List<Point>();
+            if(this.CheckLeft(currentPoint))
+            {
+                passablePoints.Add(new Point(currentPoint.X,currentPoint.Y -1));
+            }
+            if (this.CheckRight(currentPoint))
+            {
+                passablePoints.Add(new Point(currentPoint.X, currentPoint.Y + 1));
+            }
+            if (this.CheckUp(currentPoint))
+            {
+                passablePoints.Add(new Point(currentPoint.X-1, currentPoint.Y));
+            }
+            if (this.CheckDown(currentPoint))
+            {
+                passablePoints.Add(new Point(currentPoint.X + 1, currentPoint.Y));
+            }
+            return passablePoints;
+        }
+        public bool CheckLeft(Point currentPoint)
+        {
+            if(currentPoint.Y==0)
+            {
+                return false;
+            }
+            if(Matrix[currentPoint.X,(currentPoint.Y-1)]=="x")
+            {
+                return false;
+            }
+            if(markedPoints.Contains(currentPoint))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public bool CheckRight(Point currentPoint)
+        {
+            if (currentPoint.Y == (Matrix.GetLength(1)-1))
+            {
+                return false;
+            }
+            if (Matrix[currentPoint.X, (currentPoint.Y + 1)] == "x")
+            {
+                return false;
+            }
+            if (markedPoints.Contains(currentPoint))
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool CheckUp(Point currentPoint)
+        {
+            if (currentPoint.X == 0)
+            {
+                return false;
+            }
+            if (Matrix[(currentPoint.X-1), currentPoint.Y] == "x")
+            {
+                return false;
+            }
+            if (markedPoints.Contains(currentPoint))
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool CheckDown(Point currentPoint)
+        {
+            if (currentPoint.X == (Matrix.GetLength(0) - 1))
+            {
+                return false;
+            }
+            if (Matrix[(currentPoint.X+1),currentPoint.Y] == "x")
+            {
+                return false;
+            }
+            if (markedPoints.Contains(currentPoint))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}

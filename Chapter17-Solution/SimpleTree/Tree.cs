b37e0803b00e87143b46018eafc6651d34e9405a
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-03 17:43:41 +0100

    Chapter 17 add SimpleTree. Create a Tree structure class

---
+++ Chapter17-Solution/SimpleTree/Tree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTree
+{
+    class Tree<T>
+    {
+        private TreeNode<T> root;
+        public Tree(T value)
+        {
+            if(value == null)
+            {
+                throw new ArgumentNullException("Cannot insert null value!");
+            }
+            this.root = new TreeNode<T>(value);
+        }
+        public Tree(T value, params Tree<T>[] children):this(value)
+        {
+            foreach(Tree<T> child in children )
+            {
+                this.root.AddChild(child.root);
+            }
+        }
+        public TreeNode<T> Root
+        {
+            get { return this.root; }
+        }
+        /// <summary>
+        /// Traverses and prints the tree in Depth-First Search (DFS) manner
+        /// </summary>
+        /// <param name="root">the root of the tree to be traversed</param>
+        /// <param name="space">the spaces used for representation of the parent-child relation</param>
+        private void PrintDFS(TreeNode<T> root,string space)
+        {
+            if(this.root == null)
+            {
+                return;
+            }
+            Console.WriteLine(space + root.Value);
+            TreeNode<T> child = null;
+            for(int i =0; i< root.ChildrenCount;i++)
+            {
+                child = root.GetChild(i);
+                PrintDFS(child, space + " ");
+            }
+        }
+        public void TraverserDFS()
+        {
+            this.PrintDFS(this.root, string.Empty);
+        }
+    }
+}
---
+++ Chapter17-Solution/SimpleTree/TreeNode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleTree
+{
+    public class TreeNode<T>
+    {
+        
+        private bool hasParent;
+        private List<TreeNode<T>> children;
+        
+        public TreeNode(T value)
+        {
+            if(value == null)
+            {
+                throw new ArgumentNullException("Cannot insert null value!");
+            }
+            Value = value;
+            this.children = new List<TreeNode<T>>();
+        }
+        public T Value { get; set; }
+        public int ChildrenCount
+        {
+            get
+            {
+                return this.children.Count;
+            }
+        }
+        /// <summary>
+        /// Add new child to the parent node
+        /// </summary>
+        /// <param name="child"></param>
+        public void AddChild(TreeNode<T> child)
+        {
+            if (child == null)
+            {
+                throw new ArgumentNullException("Cannot insert null value!");
+            }
+            if (child.hasParent)
+            {
+                throw new ArgumentException("This node already has a parent!");
+            }
+            child.hasParent = true;
+            this.children.Add(child);
+        }
+        /// <summary>
+        /// Gets the child of the node at given index
+        /// </summary>
+        /// <param name="index">the index of the desired child</param>
+        /// <returns>the child on the given position</returns>
+        public TreeNode<T> GetChild(int index)
+        {
+            if(index>(this.ChildrenCount-1))
+            {
+                throw new ArgumentException("This node has no children at that index!");
+            }
+            return this.children[index];
+        }
+    }
+}

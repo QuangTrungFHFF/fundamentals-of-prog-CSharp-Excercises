191594a57238cb4acd85e58762286160f486fd02
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-04 20:37:02 +0100

    Chapter 17 add Exercise 7

---
+++ Chapter17-Solution/Exercise07/ShortestPath.cs
@@ -0,0 +1,78 @@
+﻿using MsdnDirectedGraph;
+using System;
+using System.Collections.Generic;
+
+namespace Exercise07
+{
+    /// <summary>
+    /// Let’s have as given a graph G(V, E) and two of its vertices x and y. Write a program that finds the shortest path between two vertices measured in number of vertices staying on the path.
+    /// </summary>
+    internal class ShortestPath
+    {
+        private static void Main(string[] args)
+        {
+            //Add nodes and some edges to the graph
+            List<Node> nodes = new List<Node>();
+            nodes.Add(new Node<int>(0));
+            nodes.Add(new Node<int>(1));
+            nodes.Add(new Node<int>(2));
+            nodes.Add(new Node<int>(3));
+            nodes.Add(new Node<int>(4));
+            nodes.Add(new Node<int>(5));
+            nodes.Add(new Node<int>(6));
+            nodes.Add(new Node<int>(7));
+            nodes.Add(new Node<int>(8));
+            nodes.Add(new Node<int>(9));
+
+            List<Edge> edges = new List<Edge>();
+            edges.Add(new Edge(nodes[0], nodes[1]));
+            edges.Add(new Edge(nodes[1], nodes[2]));
+            edges.Add(new Edge(nodes[1], nodes[0]));
+            edges.Add(new Edge(nodes[2], nodes[1]));
+            edges.Add(new Edge(nodes[7], nodes[8]));
+
+            Graph testGraph = new Graph(edges, nodes);         
+
+            //Add new edges to the graph (connecting nodes)
+            testGraph.AddEdge(testGraph.Nodes[0], testGraph.Nodes[5], true);
+            testGraph.AddEdge(testGraph.Nodes[0], testGraph.Nodes[6], true);
+            testGraph.AddEdge(testGraph.Nodes[1], testGraph.Nodes[6], true);
+            testGraph.AddEdge(testGraph.Nodes[2], testGraph.Nodes[4], true);
+            testGraph.AddEdge(testGraph.Nodes[2], testGraph.Nodes[5], true);
+            testGraph.AddEdge(testGraph.Nodes[4], testGraph.Nodes[3], true);
+            testGraph.AddEdge(testGraph.Nodes[3], testGraph.Nodes[5], true);
+            testGraph.AddEdge(testGraph.Nodes[3], testGraph.Nodes[6], true);
+
+            //Print information of the graph
+            Console.WriteLine("Inital graph:");
+            Console.WriteLine("Nodes:");
+            testGraph.Nodes.ToConsole();
+            Console.WriteLine("Edges:");
+            testGraph.Edges.ToConsole();
+            Console.WriteLine("Inbound edges of {0}:", testGraph.Nodes[1]);
+            testGraph.Nodes[1].InboundEdges.ToConsole();
+            Console.WriteLine("Outbound edges of {0}:", testGraph.Nodes[1]);
+            testGraph.Nodes[1].OutboundEdges.ToConsole();
+        }
+        public static void GetShortestPath(Node nodeX, Node nodeY)
+        {
+            if(nodeX.Graph!= nodeY.Graph)
+            {
+                Console.WriteLine("2 nodes are not in the same graph!");
+                return;
+            }
+
+        }
+    }
+    public static class EnumerableHelper
+    {
+        public static void ToConsole<T>(this IEnumerable<T> enumerable)
+        {
+            foreach (T item in enumerable)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("");
+        }
+    }
+}
--- Chapter17-Solution/MsdnDirectedGraph/Graph.cs
+++ Chapter17-Solution/MsdnDirectedGraph/Graph.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace MsdnDirectedGraph
 {
@@ -8,36 +6,55 @@ namespace MsdnDirectedGraph
     {
         public List<Edge> Edges { get; set; }
         public List<Node> Nodes { get; set; }
-        public Graph(List<Edge> edges,List<Node> nodes)
+
+        public Graph(List<Edge> edges, List<Node> nodes)
         {
             this.Edges = edges;
             this.Nodes = nodes;
-            foreach(Node node in nodes)
+            foreach (Node node in nodes)
             {
                 node.Graph = this;
             }
         }
+
         public void AddEdge(Edge edge)
         {
             this.Edges.Add(edge);
         }
+
         public void AddEdge(Node from, Node to)
         {
             this.Edges.Add(new Edge(from, to));
         }
+
+        public void AddEdge(Node from, Node to, bool isTwoWay)
+        {
+            if (isTwoWay == true)
+            {
+                this.Edges.Add(new Edge(from, to));
+                this.Edges.Add(new Edge(to, from));
+            }
+            else
+            {
+                this.Edges.Add(new Edge(from, to));
+            }
+        }
+
         public void AddNode(Node node)
         {
             this.Nodes.Add(node);
             node.Graph = this;
         }
+
         public void RemoveEdge(Edge edge)
         {
             this.Edges.Remove(edge);
         }
+
         public void RemoveNode(Node node)
         {
             this.Edges.RemoveAll(e => e.From == node || e.To == node);
-            this.Nodes.Remove(node);            
+            this.Nodes.Remove(node);
         }
     }
 }

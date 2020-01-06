708e38aa35991c8f0b67aa89dca54b1b1f802dff
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-06 13:30:51 +0100

    Chapter 17 add Exercise 16

---
+++ Chapter17-Solution/Exercise16/TestProgram.cs
@@ -0,0 +1,52 @@
+﻿using MsdnDirectedGraph;
+using System;
+using System.Collections.Generic;
+
+namespace Exercise16
+{
+    internal class TestProgram
+    {
+        /// <summary>
+        /// We have N tasks to be performed successively. We are given a list of pairs of tasks for which the second is dependent on the outcome of the first and should be executed after it.
+        /// Write a program that arranges tasks in such a way that each task is be performed after all the tasks which it depends on have been completed.
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            //Add nodes and some edges to the graph
+            List<Node> nodes = new List<Node>();
+            nodes.Add(new Node<int>(0));
+            nodes.Add(new Node<int>(1));
+            nodes.Add(new Node<int>(2));
+            nodes.Add(new Node<int>(3));
+            nodes.Add(new Node<int>(4));
+            nodes.Add(new Node<int>(5));
+
+            //Add Edge to nodes. (Each edge represents a pairs of tasks)
+            List<Edge> edges = new List<Edge>();
+            edges.Add(new Edge(nodes[5], nodes[0], 1));
+            edges.Add(new Edge(nodes[4], nodes[0], 1));
+            edges.Add(new Edge(nodes[4], nodes[1], 1));
+            edges.Add(new Edge(nodes[5], nodes[2], 1));
+            edges.Add(new Edge(nodes[2], nodes[3], 1));
+            edges.Add(new Edge(nodes[3], nodes[1], 1));
+
+            Graph testGraph = new Graph(edges, nodes);
+            TopologicalSorting sort = new TopologicalSorting(testGraph);
+            Console.WriteLine("The order: ");
+            sort.PrintStack();
+        }
+    }
+
+    public static class EnumerableHelper
+    {
+        public static void ToConsole<T>(this IEnumerable<T> enumerable)
+        {
+            foreach (T item in enumerable)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Chapter17-Solution/Exercise16/TopologicalSorting.cs
@@ -0,0 +1,129 @@
+﻿using MsdnDirectedGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise16
+{
+    internal class TopologicalSorting
+    {
+        public Stack<Node> TopologicalStack { get; private set; }
+        public List<Node> Graph { get; private set; }
+        private bool isPossible = false;
+
+        public TopologicalSorting(Graph graph)
+        {
+            this.Graph = new List<Node>();
+            SortGraphByDependance(graph);
+            TopologicalStack = new Stack<Node>();
+            this.IsCycle(graph);
+            if (this.isPossible)
+            {
+                this.Sorting();
+            }
+        }
+
+        /// <summary>
+        /// Sort the given graph by number of nodes it depends on
+        /// </summary>
+        /// <param name="graph"></param>
+        private void SortGraphByDependance(Graph graph)
+        {
+            foreach (Node n in graph.Nodes)
+            {
+                this.Graph.Add(n);
+            }
+            this.Graph = this.Graph.OrderBy(x => x.OutboundEdges.Count).ToList();
+        }
+
+        /// <summary>
+        /// Sort all nodes in the graph
+        /// </summary>
+        private void Sorting()
+        {
+            int size = Graph.Count;
+            Node current;
+            for (int i = 0; i < size; i++)
+            {
+                current = Graph[i];
+                if (!current.Visited)
+                {
+                    TopologicalSort(current);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Recursive mothod to create stack of nodes
+        /// </summary>
+        /// <param name="node"></param>
+        private void TopologicalSort(Node node)
+        {
+            if (!node.Visited)
+            {
+                node.Visited = true;
+
+                foreach (Edge e in node.OutboundEdges)
+                {
+                    TopologicalSort(e.To);
+                }
+                TopologicalStack.Push(node);
+            }
+        }
+
+        /// <summary>
+        /// Topological Sorting for a graph is not possible if the graph is not a Directed Acyclic Graph. This method check if the graph is a cyclic or acyclic graph.
+        /// </summary>
+        /// <param name="graph"></param>
+        public void IsCycle(Graph graph)
+        {
+            bool isRepeated = false;
+            Node currentNode = graph.Nodes[1];
+            var nodeQueue = new Queue<Node>();
+            nodeQueue.Enqueue(currentNode);
+            while (isRepeated != true && nodeQueue.Count > 0)
+            {
+                currentNode = nodeQueue.Dequeue();
+                if (currentNode.Visited == false)
+                {
+                    currentNode.Visited = true;
+                    foreach (Edge e in currentNode.OutboundEdges)
+                    {
+                        nodeQueue.Enqueue(e.To);
+                    }
+                }
+                else
+                {
+                    isRepeated = true;
+                }
+            }
+            if (!isRepeated)
+            {
+                this.isPossible = true;
+            }
+        }
+
+        public void PrintStack()
+        {
+            if (isPossible)
+            {
+                var cloneStack = new Stack<Node>(this.TopologicalStack);
+                var rightOrder = new List<Node>();
+                while (cloneStack.Count > 0)
+                {
+                    rightOrder.Add(cloneStack.Pop());
+                }
+                rightOrder.Reverse();
+                foreach (Node n in rightOrder)
+                {
+                    Console.Write(n.ToString() + " ");
+                }
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("Such order is not exist because the graph is cycle!");
+            }
+        }
+    }
+}

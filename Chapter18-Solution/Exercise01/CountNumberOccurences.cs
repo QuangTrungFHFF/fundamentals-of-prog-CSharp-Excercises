0d82708c2334006e41405f400f19a22cc16383d1
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-06 23:55:29 +0100

    Chapter 18 add Exercise 1

---
+++ Chapter18-Solution/Exercise01/CountNumberOccurences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Exercise01
+{
+    class Program
+    {
+        /// <summary>
+        /// Write a program that counts, in a given array of integers, the number of occurrences of each integer.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 3, 4, 4, 2, 3, 3, 4, 3, 2 };
+            var occurrencesDictionary = new Dictionary<int, int>();
+            foreach(int i in numbers)
+            {
+                int count;
+                if(!occurrencesDictionary.TryGetValue(i,out count))
+                {
+                    count = 0;
+                }
+                occurrencesDictionary[i] = count + 1;
+            }
+            foreach(var num in occurrencesDictionary)
+            {
+                Console.WriteLine("Number {0} occurs {1} time(s) in the array.", num.Key, num.Value);
+            }
+        }
+    }
+}

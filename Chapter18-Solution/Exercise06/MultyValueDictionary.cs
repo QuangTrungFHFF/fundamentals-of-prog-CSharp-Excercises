f5f77e1c6272faaf27bf3e08fec00dfabd779b90
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-07 22:55:16 +0100

    Chapter 18 add Exercise 6

---
+++ Chapter18-Solution/Exercise06/MultyValueDictionary.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Exercise06
+{
+    class MultyValueDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, List<TValue>>>
+    {
+        private Dictionary<TKey, List<TValue>> dictionary;
+        public int Count { get { return this.dictionary.Count; } }
+        public List<TValue> this[TKey key]
+        {
+            get
+            {
+                return dictionary[key];
+            }
+            set
+            {
+                dictionary[key] = value;
+            }
+        }
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                List<TValue> values = new List<TValue>();
+                foreach(var item in dictionary)
+                {
+                    values.AddRange(item.Value);
+                }
+                return values;
+            }
+        }
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                List<TKey> keys = new List<TKey>();
+                foreach (var item in dictionary)
+                {
+                    keys.Add(item.Key);
+                }
+                return keys;
+            }
+        }
+        public MultyValueDictionary()
+        {
+            this.dictionary = new Dictionary<TKey, List<TValue>>();
+        }
+        public MultyValueDictionary(TKey key, params TValue[] values)
+        {
+            this.dictionary = new Dictionary<TKey, List<TValue>>();
+            this.dictionary.Add(key, new List<TValue>(values));
+        }
+
+        public void Add(TKey key,params TValue[] newValues)
+        {
+            if(this.dictionary.ContainsKey(key))
+            {
+                this.dictionary[key].Union(newValues.ToList());
+            }
+            else
+            {
+                this.dictionary.Add(key, new List<TValue>(newValues));
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            if(dictionary.ContainsKey(key))
+            {
+                dictionary.Remove(key);
+                return true;
+            }
+            return false;
+        }
+        public void Clear()
+        {
+            this.dictionary = new Dictionary<TKey, List<TValue>>();
+        }
+        public bool TryGetValues(TKey key,out List<TValue> values)
+        {
+            if(this.dictionary.ContainsKey(key))
+            {
+                values = dictionary[key].ToList();
+                return true;
+            }
+            else
+            {
+                values = new List<TValue>();
+                return false;
+            }
+
+        }
+
+        public IEnumerator<KeyValuePair<TKey, List<TValue>>> GetEnumerator()
+        {
+            foreach(var item in dictionary)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Chapter18-Solution/Exercise06/TestProgram.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise06
+{
+    /// <summary>
+    /// Implement a hash-table, allowing the maintenance of more than one value for a specific key.
+    /// </summary>
+    class TestProgram
+    {
+        static void Main(string[] args)
+        {
+            //Create a multy values dictionary
+            var dictionary = new MultyValueDictionary<string, string>("Barack Obama", "President", "Writer", "Artist");
+            Console.WriteLine("There are {0} people on the list.",dictionary.Count);
+            //Add new key with multiple values to the dictionary
+            dictionary.Add("Bill Gates", "Inventor", "Adviser", "Entrepreneur", "Software Developer");
+            Console.WriteLine("Barack Obama: ");
+            foreach(var item in dictionary["Barack Obama"])
+            {
+                Console.Write(item + " / ");
+            }
+            Console.WriteLine("\nBill Gates: ");
+            foreach (var item in dictionary["Bill Gates"])
+            {
+                Console.Write(item + " / ");
+            }
+            //Try Remove function
+            dictionary.Remove("Barack Obama");
+            //TryGetValues function
+            List<string> items; 
+            if(dictionary.TryGetValues("Barack Obama",out items))
+            {
+                foreach (var item in items)
+                {
+                    Console.Write(item + " / ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNot found!");
+            }
+
+            if (dictionary.TryGetValues("Bill Gates", out items))
+            {
+                
+                foreach (var item in items)
+                {
+                    Console.Write(item + " / ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not found!");
+            }
+        }
+    }
+}

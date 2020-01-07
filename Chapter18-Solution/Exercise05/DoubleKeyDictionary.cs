a82a50dd7bbc400a462b2624f7a57e9e699051c5
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-07 20:09:26 +0100

    Chapter 18 add Exercise 5

---
+++ Chapter18-Solution/Exercise05/DoubleKeyDictionary.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Exercise05
+{
+    internal class DoubleKeyDictionary<TKey1, TKey2, TValue> : IEnumerable<Tuple<TKey1, TKey2, TValue>>
+    {
+        private Dictionary<ValueTuple<TKey1, TKey2>, TValue> dictionary;
+
+        //public ValueTuple<TKey1, TKey2> Key { get; set; }
+        public int Count { get { return this.dictionary.Count; } }
+
+        public DoubleKeyDictionary()
+        {
+            this.dictionary = new Dictionary<(TKey1, TKey2), TValue>();
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TValue value)
+        {
+            if (!ContainsKeys(key1, key2))
+            {
+                this.dictionary.Add(ValueTuple.Create(key1, key2), value);
+            }
+            else
+            {
+                Console.Error.WriteLine("An item with the same two keys has already been added.");
+            }
+        }
+
+        public bool ContainsKeys(TKey1 key1, TKey2 key2)
+        {
+            var key = ValueTuple.Create(key1, key2);
+            if (!dictionary.ContainsKey(key))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool TryGetValue(TKey1 key1, TKey2 key2, out TValue value)
+        {
+            if (ContainsKeys(key1, key2))
+            {
+                value = dictionary[(key1, key2)];
+                return true;
+            }
+            else
+            {
+                value = default(TValue);
+                return false;
+            }
+        }
+
+        public void Clear()
+        {
+            dictionary.Clear();
+        }
+
+        public bool Remove(TKey1 key1, TKey2 key2)
+        {
+            if (ContainsKeys(key1, key2))
+            {
+                dictionary.Remove((key1, key2));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerator<Tuple<TKey1, TKey2, TValue>> GetEnumerator()
+        {
+            foreach (var item in dictionary)
+            {
+                yield return Tuple.Create(item.Key.Item1, item.Key.Item2, item.Value);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Tuple<TKey1, TKey2, TValue>>)this).GetEnumerator();
+        }
+    }
+}
---
+++ Chapter18-Solution/Exercise05/TestProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercise05
+{
+    internal class TestProgram
+    {
+        private static void Main(string[] args)
+        {
+            //Create a new Double Key Dictionary
+            DoubleKeyDictionary<string, int, double> classRoom = new DoubleKeyDictionary<string, int, double>();
+            classRoom.Add("Mary Barra", 16, 2.2);
+            classRoom.Add("Vladimir Putin", 19, 3.2);
+            classRoom.Add("Bill Gates", 18, 1.2);
+            classRoom.Add("Steven Jobs", 16, 2.2);
+            classRoom.Add("Angela Merkel", 15, 1.7);
+            classRoom.Add("Barack Obama", 16, 1.9);
+            //Print to the console
+            Console.WriteLine("There are {0} student(s) in the class.", classRoom.Count);
+            foreach (var student in classRoom)
+            {
+                Console.WriteLine("Name: {0} - Age: {1} - Score: {2}", student.Item1, student.Item2, student.Item3);
+            }
+            //Try adding an already exist pair of keys
+            classRoom.Add("Angela Merkel", 15, 4.0);
+            //Test remove function
+            classRoom.Remove("Angela Merkel", 15);
+            classRoom.Remove("Angela Merkel", 19);
+            //Print to the console
+            foreach (var student in classRoom)
+            {
+                Console.WriteLine("Name: {0} - Age: {1} - Score: {2}", student.Item1, student.Item2, student.Item3);
+            }
+            //Test TryGetValue function
+            double score;
+            if (classRoom.TryGetValue("Barack Obama", 16, out score))
+            {
+                Console.WriteLine("Score: " + score);
+            }
+            else
+            {
+                Console.WriteLine("Not found!");
+            }
+        }
+    }
+}

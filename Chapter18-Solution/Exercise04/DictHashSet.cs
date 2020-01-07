ff5d57c55e1a1faa9b1e685bf60ee0f6aade6b51
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-07 15:42:34 +0100

    Chapter 18 add Exercise 4

---
+++ Chapter18-Solution/Exercise04/DictHashSet.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Exercise04
+{
+    class DictHashSet<T> : IEnumerable<T>
+    {
+        private Dictionary<T, T> valuePairs;
+        public int Count { get { return this.valuePairs.Count; } }
+        public DictHashSet()
+        {
+            this.valuePairs = new Dictionary<T, T>();
+        }
+        public bool Add(T value)
+        {
+            T item;
+            if(this.valuePairs.TryGetValue(value,out item))
+            {
+                return false;
+            }
+            else
+            {
+                this.valuePairs[value] = value;
+                return true;
+            }
+        }
+        public bool Contain(T value)
+        {
+            T item;
+            if (this.valuePairs.TryGetValue(value, out item))
+            {
+                return true;
+            }
+            else
+            {                
+                return false;
+            }
+        }
+        public void Clear()
+        {
+            this.valuePairs.Clear();
+            Console.WriteLine("The DictHashset is now empty!");
+        }
+        /// <summary>
+        /// Copies the elements of a HashSet<T> collection to an array.
+        /// </summary>
+        /// <returns></returns>
+        public T[] CopyTo()
+        {
+            if(this.Count ==0)
+            {
+                throw new ArgumentNullException("There is no items in the set!");
+            }
+            return this.valuePairs.Keys.ToArray();
+        }
+        /// <summary>
+        /// true if the element is successfully found and removed; otherwise, false.
+        /// This method returns false if item is not found in the set.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Remove(T value)
+        {
+            if(this.Contain(value))
+            {
+                this.valuePairs.Remove(value);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Modifies the current DictHashSet<T> object to contain all elements that are present in itself, the specified collection, or both.
+        /// </summary>
+        /// <param name="other">The collection to compare to the current DictHashSet<T> object.</param>
+        public void UnionWith(DictHashSet<T> other)
+        {
+            if(other == null)
+            {
+                Console.WriteLine("Cannot union with null DictHashSet!");
+            }
+            else
+            {
+                foreach (var value in other)
+                {
+                    this.Add(value);
+                }
+            }
+        }
+        /// <summary>
+        /// Modifies the current DictHashSet<T> object to contain only elements that are present in that object and in the specified collection.
+        /// </summary>
+        /// <param name="other"></param>
+        public void IntersectWith(DictHashSet<T> other)
+        {
+            if(other == null)
+            {
+                Console.WriteLine("Cannot intersect with null DictHashSet!");
+            }
+            else
+            {
+                foreach(var item in this)
+                {
+                    if(!other.Contain(item))
+                    {
+                        this.Remove(item);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Removes all elements in the specified collection from the current DictHashSet<T> object.
+        /// </summary>
+        /// <param name="other"></param>
+        public void ExceptWith(DictHashSet<T> other)
+        {
+            if (other == null)
+            {
+                Console.WriteLine("Cannot intersect with null DictHashSet!");
+            }
+            else
+            {
+                foreach (var item in this)
+                {
+                    if (other.Contain(item))
+                    {
+                        this.Remove(item);
+                    }
+                }
+            }
+        }
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(var item in this.valuePairs)
+            {
+                yield return item.Key;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Chapter18-Solution/Exercise04/TestProgram.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Exercise04
+{
+    /// <summary>
+    /// Implement a DictHashSet<T> class, based on HashDictionary<K, V> class, we discussed in the text above.
+    /// </summary>
+    internal class TestProgram
+    {
+        private static void Main(string[] args)
+        {
+            //Create a DictHashSet<T>
+            DictHashSet<int> numbers = new DictHashSet<int>();
+            //Add items to DictHashSet
+            Console.WriteLine("Add new items");
+            Console.WriteLine(numbers.Add(0));
+            Console.WriteLine(numbers.Add(1));
+            Console.WriteLine(numbers.Add(2));
+            Console.WriteLine(numbers.Add(100));
+            Console.WriteLine(numbers.Add(200));
+            Console.WriteLine(numbers.Add(300));
+            Console.WriteLine(numbers.Add(400));
+            Console.WriteLine(numbers.Add(500));
+            Console.WriteLine(numbers.Add(600));
+            //Add an already exist item
+            Console.WriteLine("Add an already exist items");
+            Console.WriteLine(numbers.Add(2));
+            Console.WriteLine(numbers.Add(200));
+            //Remove items
+            Console.WriteLine("Remove items");
+            Console.WriteLine(numbers.Remove(2));
+            Console.WriteLine(numbers.Remove(99));
+            //Check if the set contains certain items
+            Console.WriteLine("Check if the set contains certain items");
+            Console.WriteLine(numbers.Remove(0));
+            Console.WriteLine(numbers.Remove(1));
+            Console.WriteLine(numbers.Remove(3));
+            //Copy the DictHashSet to an array
+            Console.WriteLine("Copy the DictHashSet to an array");
+            var numberArray = numbers.CopyTo();
+            foreach (var i in numberArray)
+            {
+                Console.Write(i + " ");
+            }
+            //Clear the set
+            Console.WriteLine("\nClear the set.");
+            numbers.Clear();
+            //Test Union Function
+            Console.WriteLine("Test Union Function");
+            var newSet = new DictHashSet<int>();
+            foreach (int i in numberArray)
+            {
+                newSet.Add(i);
+            }
+            numbers.UnionWith(newSet);
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            //Test Intersect Function
+            Console.WriteLine("\nTest Intersect Function.");
+            newSet.Remove(100);
+            newSet.Add(2000);
+            numbers.IntersectWith(newSet);
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            //Test Except Function
+            Console.WriteLine("\nTest Except Function.");
+            numbers.Add(3000);
+            numbers.Add(4000);
+            numbers.ExceptWith(newSet);
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}

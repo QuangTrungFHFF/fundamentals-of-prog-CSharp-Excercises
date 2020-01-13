d5dac40f9c2c7c6553866f5291b5c7c0a0c0a86c
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-13 23:59:03 +0100

    Chapter 18 update Exercise 13

---
+++ Chapter18-Solution/Exercise13/Sequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise13
+{
+    class Sequence<T> : IComparable<Sequence<T>>
+    {
+        private List<T> container;
+        public int Count { get { return this.container.Count; } }
+        public Sequence(List<T> sequence)
+        {
+            this.container = new List<T>(sequence);
+        }
+        int IComparable<Sequence<T>>.CompareTo(Sequence<T> other)
+        {
+            Sequence<T> sequence = (Sequence<T>)other;
+            return this.Count.CompareTo(other.Count);
+        }
+    }
+}

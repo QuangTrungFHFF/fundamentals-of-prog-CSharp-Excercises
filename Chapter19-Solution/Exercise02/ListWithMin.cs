07691ec18f77ea371433b78f0d7c56410c3c2a74
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-14 16:19:45 +0100

    Chapter 19 add Exercise 2

---
+++ Chapter19-Solution/Exercise02/ListWithMin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise02
+{
+    class ListWithMin<T> where T : IComparable
+    {
+        private List<T> list;
+        public T Min { get; private set; }
+        
+        public ListWithMin()
+        {
+            this.list = new List<T>();
+        }
+        public ListWithMin(List<T> values)
+        {
+            values.Sort();
+            this.list = new List<T>(values);
+            this.Min = list[0];
+        }
+        public ListWithMin(T value)
+        {
+            this.list = new List<T>();
+            this.list.Add(value);
+            this.Min = value;
+        }
+        public void Add(T value)
+        {
+            this.list.Add(value);
+            if(this.Min == null)
+            {
+                this.Min = value;
+                return;
+            }
+            if (this.Min.CompareTo(value) ==1)
+            {
+                this.Min = value;
+            }
+        }
+    }
+}
---
+++ Chapter19-Solution/Exercise02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise02
+{
+    /// <summary>
+    /// Implement a data structure, which can quickly do the following two operations: add an element and extract the smallest element.
+    /// The structure should accept adding duplicated elements.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ListWithMin<int>(125);
+            list.Add(24);
+            list.Add(78);
+            list.Add(25);
+            list.Add(36);
+            list.Add(78);
+            list.Add(965);
+            Console.WriteLine("Min element is: {0}", list.Min);
+        }
+    }
+}

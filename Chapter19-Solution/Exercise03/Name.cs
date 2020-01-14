8e16108d5ce164bfbc1b8c26d1363034d0cd4e61
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-14 20:08:13 +0100

    Chapter 19 add Exercise 3

---
+++ Chapter19-Solution/Exercise03/Name.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise03
+{
+    class Name: IComparable
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public Name(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+        public override string ToString()
+        {
+            return string.Format(this.FirstName + " " + this.LastName);
+        }
+        public int CompareTo(object that)
+        {
+            var n = (Name)that;
+            if(this.LastName.CompareTo(n.LastName)==0)
+            {
+                return this.FirstName.CompareTo(n.FirstName);
+            }
+            return this.LastName.CompareTo(n.LastName);
+        }
+    }
+}
---
+++ Chapter19-Solution/Exercise03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exercise03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var student = new SortedDictionary<string, SortedSet<Name>>();
+            string path = Path.Combine((Path.GetFullPath(@"..\..\..\..\") + @"Textfiles\"), "student.txt");
+            try
+            {
+                using StreamReader streamReader = new StreamReader(path);
+                string line = String.Empty;
+                string[] words;
+                string[] fullName;
+
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    words = line.Split("|", StringSplitOptions.RemoveEmptyEntries);
+                    fullName = words[0].Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    if (!student.ContainsKey(words[1]))
+                    {
+                        student.Add(words[1], new SortedSet<Name>());
+                    }
+                    student[words[1]].Add(new Name(fullName[0], fullName[1]));
+                }
+
+                Print(student);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine($"Cannot find the file at: {path}");
+            }
+            catch (IOException ex)
+            {
+                Console.Error.WriteLine($"Error trying reading the file: {path}");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void Print(SortedDictionary<string, SortedSet<Name>> student)
+        {
+            foreach (var s in student)
+            {
+                Console.WriteLine(s.Key + ": ");
+                foreach (var n in s.Value)
+                {
+                    Console.Write("\t" + n.ToString());
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}

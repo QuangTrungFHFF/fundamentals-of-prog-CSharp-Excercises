ec859c17cc9c98c6f9f30c30a8a7bc1d1be40aa5
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-14 22:52:33 +0100

    Chapter 19 add Exercise 5

---
+++ Chapter19-Solution/Exercise05/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise05
+{
+    class Product : IComparable
+    {
+        public string Barcode { get; private set; }
+        public string Producer { get; private set; }
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public Product(string barCode, string producer, string name, double price)
+        {
+            this.Barcode = barCode;
+            this.Producer = producer;
+            this.Price = price;
+            this.Name = name;
+        }
+        public int CompareTo(object obj)
+        {
+            var that = (Product)obj;
+            if(this.Price.CompareTo(that.Price) == 0)
+            {
+                return this.Barcode.CompareTo(that.Barcode);
+            }
+            return this.Price.CompareTo(that.Price);
+        }
+    }
+}

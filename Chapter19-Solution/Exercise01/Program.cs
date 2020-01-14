e816631812b5e2292742ecbd534eaf81ad4bd82f
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-14 15:56:17 +0100

    Chapter 19 add Exercise 1

---
+++ Chapter19-Solution/Exercise01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise01
+{
+    /// <summary>
+    /// Hash-tables do not allow storing more than one value in a key.
+    /// How can we get around this restriction? Define a class to hold multiple values in a hash-table.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash-Table with multy value.");
+            var dictionary = new Dictionary<string, ValuesHolder<string>>();
+            dictionary.Add("Mary", new ValuesHolder<string>("01524 9658745", "01522 6855874", "01632 9632154", "01782 1455214"));
+            dictionary.Add("Peter", new ValuesHolder<string>("01584 5858745", "01522 6895474", "01247 9856325", "01712 8745854"));
+            dictionary["Mary"].Add("01254 2541254");
+            foreach(var student in dictionary)
+            {
+                Console.WriteLine("Name: " + student.Key);
+                foreach(var phoneNumber in student.Value)
+                {
+                    Console.WriteLine("      {0}", phoneNumber);
+                }
+            }
+        }
+    }
+}
---
+++ Chapter19-Solution/Exercise01/ValuesHolder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise01
+{
+    class ValuesHolder<T> : IEnumerable<T>
+    {
+        public List<T> Values { get; private set; }
+        public int Count { get { return this.Values.Count; } }
+        public ValuesHolder()
+        {
+            this.Values = new List<T>();
+        }
+        public ValuesHolder(params T[] values)
+        {
+            this.Values = new List<T>();
+            foreach (T value in values)
+            {
+                this.Values.Add(value);
+            }
+        }
+        public void Add(params T[] values)
+        {
+            foreach(T value in values)
+            {
+                this.Values.Add(value);
+            }
+        }
+        public void Remove(T value)
+        {
+            if(value != null)
+            {
+                this.Values.Remove(value);
+            }
+        }
+        public void Clear()
+        {
+            this.Values = new List<T>();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach(T item in this.Values)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}

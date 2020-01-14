75c1dc14cd02d5599eef256185b64faa01ecd0b9
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-14 22:38:15 +0100

    Chapter 19 add Exercise 4

---
+++ Chapter19-Solution/Exercise04/BiDictionary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise04
+{
+    class BiDictionary<K1,K2,T> 
+    {
+        private Dictionary<K1, List<T>> firstKey;
+        private Dictionary<K2, List<T>> secondKey;
+        public BiDictionary()
+        {
+            this.firstKey = new Dictionary<K1, List<T>>();
+            this.secondKey = new Dictionary<K2, List<T>>();
+        }
+        public void Add(K1 key1, K2 key2, T value)
+        {
+            if(!this.firstKey.ContainsKey(key1))
+            {
+                this.firstKey.Add(key1, new List<T>());
+            }
+            if (!this.secondKey.ContainsKey(key2))
+            {
+                this.secondKey.Add(key2, new List<T>());
+            }
+            this.firstKey[key1].Add(value);
+            this.secondKey[key2].Add(value);
+        }
+        public void SearchByFirstKey(K1 key1)
+        {
+            List<T> values;
+            if(!this.firstKey.TryGetValue(key1,out values))
+            {
+                Console.WriteLine("Key \"{0}\" not found!", key1.ToString());
+                return;
+            }
+            Console.WriteLine("{0} : ", key1.ToString());
+            foreach(var v in values)
+            {
+                Console.WriteLine(v.ToString() + " ");
+            }
+        }
+        public void SearchBySecondKey(K2 key2)
+        {
+            List<T> values;
+            if (!this.secondKey.TryGetValue(key2, out values))
+            {
+                Console.WriteLine("Key \"{0}\" not found!", key2.ToString());
+                return;
+            }
+            Console.WriteLine("{0} : ", key2.ToString());
+            foreach (var v in values)
+            {
+                Console.WriteLine(v.ToString() + " ");
+            }
+        }
+        public void Search(K1 key1,K2 key2)
+        {
+            List<T> first;
+            List<T> second;
+            if (!this.firstKey.TryGetValue(key1, out first)|| !this.secondKey.TryGetValue(key2,out second))
+            {
+                Console.WriteLine("Values by keys \"{0},{1}\" not found!",key1.ToString(), key2.ToString());
+                return;
+            }
+            List<T> values = new List<T>();
+            foreach(var item in first)
+            {
+                if(second.Contains(item))
+                {
+                    values.Add(item);
+                }
+            }
+            Console.WriteLine("{0},{1} : ",key1.ToString(), key2.ToString());
+            foreach (var v in values)
+            {
+                Console.WriteLine(v.ToString() + " ");
+            }
+        }
+    }
+}
---
+++ Chapter19-Solution/Exercise04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise04
+{
+    /// <summary>
+    /// Implement a class BiDictionary<K1,K2,T>, which allows adding triplets {key1, key2, value} and quickly search
+    /// by either of the keys key1, key2 as well as searching by combination of the both keys.
+    /// Note: Adding many elements with the same keys is allowed.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new BiDictionary<string, int, double>();
+            dictionary.Add("Sakura", 16, 2.1);
+            dictionary.Add("Katsu", 17, 1.6);
+            dictionary.Add("Ito", 18, 2.1);
+            dictionary.Add("Sakura", 15, 1.8);
+            dictionary.Add("Hotaka", 18, 1.9);
+            dictionary.Add("Madoka", 17, 4.0);
+            dictionary.Add("Sakura", 15, 3.1);
+            dictionary.Add("Ito", 20, 2.7);
+            dictionary.Add("Sakura", 15, 1.1);
+            dictionary.Add("Katsu", 15, 1.3);
+            dictionary.Add("Madoka", 17, 1.1);
+            dictionary.Add("Madoka", 15, 2.1);
+            //Search by first key
+            dictionary.SearchByFirstKey("Sakura");
+            //Search by second key
+            dictionary.SearchBySecondKey(18);
+            //Search by both keys
+            dictionary.Search("Sakura", 15);
+        }
+    }
+}

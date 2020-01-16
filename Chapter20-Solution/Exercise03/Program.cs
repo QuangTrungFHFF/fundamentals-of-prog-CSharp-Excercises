60a1b188c833fcfc8696c5d9851749364c5a104d
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-16 14:44:37 +0100

    Chapter 20 add Exercise 3

---
+++ Chapter20-Solution/Exercise03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise03
+{
+    /// <summary>
+    /// Initialize an array of 10 students and sort them by mark in ascending order. Use the interface System.IComparable<T>.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Mary", "Oma1", 9));
+            students.Add(new Student("Mary", "Oma2", 2));
+            students.Add(new Student("Mary", "Oma3", 4));
+            students.Add(new Student("Mary", "Oma4", 6.2));
+            students.Add(new Student("Mary", "Oma5", 3.5));
+            students.Add(new Student("Mary", "Oma6", 7.2));
+            students.Add(new Student("Mary", "Oma7", 9.6));
+            students.Add(new Student("Mary", "Oma8", 7.5));
+            students.Add(new Student("Mary", "Oma9", 8.3));
+            students.Add(new Student("Mary", "OmaX", 5.5));
+            students.Sort();
+            foreach(var s in students)
+            {
+                Console.WriteLine(s.ToString());
+            }
+        }
+    }
+}
---
+++ Chapter20-Solution/Exercise03/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Exercise03
+{
+    class Student : Human,IComparable<Student>
+    {
+        public string Name { get { return string.Format($"{FirstName} {LastName}"); } }
+        public double Mark { get; set; }
+        public Student(string firstName,string lastName, double mark) : base(firstName,lastName)
+        {
+            this.Mark = mark;            
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (this.GetType() != obj.GetType())
+                return false;
+            var that = (Student)obj;
+            if (this.GetHashCode() == that.GetHashCode())
+                return true;
+            return false;
+
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Mark.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return string.Format($"Name - {Name.PadRight(15)} | Mark - {Mark:N2}");
+        }
+
+        public int CompareTo([AllowNull] Student other)
+        {
+            if (other == null)
+                return -1;
+            return this.Mark.CompareTo(other.Mark);
+        }
+    }
+}

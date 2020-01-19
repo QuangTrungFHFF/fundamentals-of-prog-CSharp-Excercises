a7e27bac4e5f00f81c4156603e2d80b5dcb684ac
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-19 23:07:08 +0100

    Chapter 14 rename folder

---
+++ Chapter20-Solution/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Directory.CreateDirectory(@"Output");
+
+        }
+    }
+}

3ec90323a14b9f70d2013a41d4ada43520886a88
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-21 19:15:09 +0100

    Chapter 21 add Exercise 1

---
+++ Chapter21-HighQualityProgrammingCode/RefactorCode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RefactorCode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int value = 10;
+            int i = 0;
+            switch (value)
+            {
+                case 10:
+                    int w = 5;
+                    Console.WriteLine(w);
+                    break;
+
+                case 9:
+                    i = 0;
+                    break;
+
+                case 8:
+                    Console.WriteLine(8);
+                    break;
+
+                default:
+                    Console.WriteLine("def");
+                    {
+                        Console.WriteLine("hoho");
+                    }
+                    for (int k = 0; k < i; k++)
+                    {
+                        Console.WriteLine(k - 'f');
+                    }
+                    break;
+            }
+            Console.WriteLine("loop!");
+        }
+    }
+}

f48123ee14b83cb571d9235c51caed17db7ee994
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-22 21:39:23 +0100

    Chapter 22 add Exercise 5

--- Chapter22-LINQ/FindStudentWithGivenAgeRange/Program.cs
+++ Chapter22-LINQ/FindStudentWithGivenAgeRange/Program.cs
@@ -12,7 +12,7 @@ namespace FindStudentWithGivenAgeRange
             var students = new List<Student>() { new Student("Fredrick", "Conley", 16), new Student("Barack", "Obama", 26), new Student("Tyra", "Tang", 17),
                new Student("Aliesha", "Pittman", 16), new Student("Charley", "Shannon", 26), new Student("Zaria", "Toby", 18),new Student("Abby", "Martin", 19),
                new Student("Neil", "Krueger", 26), new Student("Alexandru", "Glass", 17), new Student("Connie", "Boyce", 20), new Student("Reegan", "Forster", 26)};
-            var studentWithAgeFrom18To24 = students.Where(s => (s.Age > 18 && s.Age < 24)).ToList();
+            var studentWithAgeFrom18To24 = students.Where(s => (s.Age > 17 && s.Age < 25)).ToList();
             foreach (Student s in studentWithAgeFrom18To24)
             {
                 Console.WriteLine(s.ToString());
---
+++ Chapter22-LINQ/StudentsSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using StudentLib;
+using System.Linq;
+
+namespace StudentsSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>() { new Student("Fredrick", "Conley", 16), new Student("Barack", "Obama", 26), new Student("Tyra", "Tang", 17),
+               new Student("Aliesha", "Pittman", 16), new Student("Charley", "Shannon", 26), new Student("Zaria", "Toby", 18),new Student("Abby", "Martin", 19),
+               new Student("Neil", "Krueger", 26), new Student("Alexandru", "Glass", 17), new Student("Connie", "Boyce", 20), new Student("Reegan", "Forster", 26)};
+
+            var sortedStudentList = students.OrderByDescending(x => x.FirstName).ThenByDescending(x => x.LastName).ToList();
+            foreach (var s in sortedStudentList)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+            Console.WriteLine("-------------------------");
+
+            var sortedStudentListByLinq = from student in students
+                                          orderby student.FirstName descending, student.LastName descending
+                                          select student;
+
+            foreach (var s in sortedStudentListByLinq)
+            {
+                Console.WriteLine(s.ToString());
+            }
+        }
+    }
+}

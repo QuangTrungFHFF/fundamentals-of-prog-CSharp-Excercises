e9b9d4fae101a0d503f705724e26146c76c7ea73
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-22 21:06:39 +0100

    Chapter 22 add new Library

---
+++ Chapter22-LINQ/FindStudentWithGivenAgeRange/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Student;
+
+namespace FindStudentWithGivenAgeRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Student.Student a = new Student.Student()
+        }
+    }
+}
---
+++ Chapter22-LINQ/StudentLib/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace StudentLib
+{
+    public class Student : IComparable<Student>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Student(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+        public int CompareTo([AllowNull] Student other)
+        {
+            if (this.FirstName.CompareTo(other.FirstName) == 0)
+            {
+                if (this.LastName.CompareTo(other.LastName) == 0)
+                {
+                    return this.Age.CompareTo(other.Age);
+                }
+                return this.LastName.CompareTo(other.LastName);
+            }
+            return this.FirstName.CompareTo(other.FirstName);
+        }
+        public override bool Equals(object obj)
+        {
+            var that = (Student)obj;
+            int a = this.FirstName.CompareTo(that.FirstName);
+            int b = this.LastName.CompareTo(that.LastName);
+            int c = this.Age.CompareTo(that.Age);
+            return (a * b * c == 0);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = this.FirstName.GetHashCode();
+            hash += this.LastName.GetHashCode() * 2;
+            hash += this.Age.GetHashCode() * 3;
+            return hash;
+        }
+    }
+}

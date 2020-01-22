85dc9e0c5d60bd28d0b96d238aa8f2f1a936ad7d
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-22 20:47:08 +0100

    Chapter 22 add Exercise 3

---
+++ Chapter22-LINQ/Student/FindStudents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Student
+{
+    public class FindStudents
+    {
+        public static List<Student> GetStudentsWithFirstNameBeforeLastNameInAlphabetically(List<Student> students)
+        {
+            var result = from student in students
+                         where (student.FirstName[0].CompareTo(student.LastName[0]) < 0)
+                         select student;
+            return result.ToList();
+        }
+    }
+}
---
+++ Chapter22-LINQ/UnitTestProject/FindStudentsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Student;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student.Tests
+{
+    [TestClass()]
+    public class FindStudentsTests
+    {
+        [TestMethod()]
+        public void GetStudentsWithFirstNameBeforeLastNameInAlphabeticallyTest()
+        {
+            //Arrange
+            var students = new List<Student>() {new Student("Abby","Martin",16), new Student("Barack", "Obama", 26), new Student("Zaria", "Toby", 17)};            
+            var expectedResult = new List<Student>() { new Student("Abby", "Martin", 16), new Student("Barack", "Obama", 26) };
+            //Act
+            var actualResult = FindStudents.GetStudentsWithFirstNameBeforeLastNameInAlphabetically(students);
+            int a = actualResult.Count;
+            //Assert            
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}

bbd5b9a921751778608224622072cc915d6834bc
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-22 18:38:55 +0100

    Chapter 22 add Exercise 2 - Sum<T> method

---
+++ Chapter22-LINQ/InterfaceExtension/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public static class IEnumerableExtension
+    {
+        public static double Sum<T>(this IEnumerable<T> elements)
+            where T:IComparable<T>
+        {
+            double result = 0;
+            try
+            {
+                foreach (var e in elements)
+                {
+                    result += Convert.ToDouble(e);
+                }
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("Element cannot be sum!");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Cannot convert string element to default format due to incorrect format!");
+            }
+            return result;    
+        }
+    }
+}

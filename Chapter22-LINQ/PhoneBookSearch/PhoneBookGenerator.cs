9940810cff8e0525c430240f36791c13433e7004
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-23 15:01:20 +0100

    Chapter 22 add Exercise 8 phonebook generator

---
+++ Chapter22-LINQ/PhoneBookSearch/PhoneBookGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RandomNameGeneratorLibrary;
+using System.Linq;
+
+namespace PhoneBookSearch
+{
+    class PhoneBookGenerator
+    {
+        private HashSet<string> names;
+        private HashSet<string> phoneNumbers;
+        public PhoneBookGenerator()
+        {
+            this.names = new HashSet<string>();
+            this.phoneNumbers = new HashSet<string>();
+        }
+
+        public Dictionary<string,string> Generator(int number)
+        {
+            var phoneBook = new Dictionary<string, string>();
+            GetNameList(number);
+            GetPhonesList(number);
+            var names = this.names.ToList();
+            var phoneNumbers = this.phoneNumbers.ToList();
+            for(int i =0; i< number; i++)
+            {
+                phoneBook.Add(names[i], phoneNumbers[i]);
+            }
+            return phoneBook;
+        }
+        private void GetNameList(int number)
+        {
+            var personGenerator = new PersonNameGenerator();
+            while (this.names.Count < number+1)
+            {
+                var newName = personGenerator.GenerateRandomFirstAndLastName();
+                this.names.Add(newName);
+            }
+        }
+        private void GetPhonesList(int number)
+        {
+            Random rnd = new Random();
+            string phoneNumber = "+49";
+            int firstPart;
+            int lastPart;
+            while (this.phoneNumbers.Count < number+1)
+            {
+                firstPart = rnd.Next(1500, 1999);
+                lastPart = rnd.Next(0000000, 9999999);
+                this.phoneNumbers.Add($"{phoneNumber}{firstPart}{lastPart}");
+            }
+        }
+    }
+}
---
+++ Chapter22-LINQ/PhoneBookSearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PhoneBookSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBookGenerator kk = new PhoneBookGenerator();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var ex = kk.Generator(50000);
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds/(double)1000} s");
+
+            PrintDictionary(ex);
+
+        }
+        public static void PrintDictionary(Dictionary<string,string> phoneBook)
+        {
+            foreach(var p in phoneBook)
+            {
+                Console.WriteLine($"{p.Key.PadRight(25)} - {p.Value}");
+            }
+        }
+
+    }
+}

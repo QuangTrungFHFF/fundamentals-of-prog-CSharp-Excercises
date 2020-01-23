9a564a4ea58bfbb78e41e322d3488277ce6cb968
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-23 10:06:10 +0100

    Chapter 22 add Exercise 7

---
+++ Chapter22-LINQ/StringCapitalization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace StringCapitalization
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var text = "i absolutely will not back down!";
+            Console.WriteLine(text.Capitalize());
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string Capitalize(this string text)
+        {
+            CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+            TextInfo info = culture.TextInfo;
+            string capitalized = info.ToTitleCase(text);
+            return capitalized;
+        }
+    }
+}

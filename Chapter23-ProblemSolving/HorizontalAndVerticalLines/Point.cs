0d62b5c459b91a6190bb7eac93c4e608785265d0
QuangTrung <tranquangtrung.vgu@gmail.com>
2020-01-25 11:37:24 +0100

    Chapter 23 add Exercise 1

---
+++ Chapter23-ProblemSolving/HorizontalAndVerticalLines/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HorizontalAndVerticalLines
+{
+    class Point
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override bool Equals(object obj)
+        {
+            var that = (Point)obj;
+            bool isEqual = (this.X.Equals(that.X)) && (this.Y.Equals(that.Y));
+            return isEqual ;
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 0;
+                hash += (hash * 397) ^ this.X.GetHashCode();
+                hash += (hash * 397) ^ this.Y.GetHashCode();
+                return hash;
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format($"({this.X}, {this.Y})");
+        }
+    }
+}

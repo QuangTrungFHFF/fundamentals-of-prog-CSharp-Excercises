3d16ab1cc4a75d1a078be0c2f2fb7c55d20eeee4
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-26 23:22:26 +0100

    Chapter 23 Exercise 1 add Plane method

---
+++ Chapter23-ProblemSolving/HorizontalAndVerticalLines/Plane.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HorizontalAndVerticalLines
+{
+    class Plane
+    {
+        public HashSet<Point> Points { get; private set; }
+        public Plane()
+        {
+            this.Points = new HashSet<Point>();
+        }
+        public void AddPoint(int x, int y)
+        {
+            this.Points.Add(new Point(x, y));
+        }
+        public void RemovePoint(int x, int y)
+        {
+            var point = new Point(x, y);
+            if(this.Points.Contains(point))
+            {
+                this.Points.Remove(point);
+            }
+        }
+        public void PrintAllPoints()
+        {
+            foreach(var p in this.Points)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+}
--- Chapter23-ProblemSolving/HorizontalAndVerticalLines/Program.cs
+++ Chapter23-ProblemSolving/HorizontalAndVerticalLines/Program.cs
@@ -10,6 +10,13 @@ namespace HorizontalAndVerticalLines
             var pointB = new Point(4, 6);
             Console.WriteLine(pointA);
             Console.WriteLine(pointA.Equals(pointB));
+
+            var plane = new Plane();
+            plane.AddPoint(4, 6);
+            plane.AddPoint(4,6);
+            plane.PrintAllPoints();
+            plane.RemovePoint(4, 6);
+            plane.PrintAllPoints();
         }
     }
 }

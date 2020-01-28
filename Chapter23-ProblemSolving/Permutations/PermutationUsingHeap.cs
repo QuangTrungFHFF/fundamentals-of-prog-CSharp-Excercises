a1968a20205eaf69bd451b1dad4f5c4d9fa5f4b6
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-01-28 14:58:15 +0100

    Chapter 23 add Exercise 2

---
+++ Chapter23-ProblemSolving/Permutations/PermutationUsingHeap.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Permutations
+{
+    public class PermutationUsingHeap
+    {
+        public static bool ForAllPermutation<T>(T[] items, Func<T[],bool> funcExecuteAndTellIfShouldStop)
+        {
+            int countOfItems = items.Length;
+
+            if(countOfItems <=1)
+            {
+                return funcExecuteAndTellIfShouldStop(items);
+            }
+
+            var index = new int[countOfItems];
+            for(int i =0; i<countOfItems;i++)
+            {
+                index[i] = 0;
+            }
+
+            if(funcExecuteAndTellIfShouldStop(items))
+            {
+                return true;
+            }
+
+            for(int i =1;i<countOfItems;)
+            {
+                if(index[i]<i)
+                {
+                    if(i%2 ==1)
+                    {
+                        Swap(ref items[i], ref items[index[i]]);
+                    }
+                    else
+                    {
+                        Swap(ref items[i], ref items[0]);
+                    }
+
+                    if(funcExecuteAndTellIfShouldStop(items))
+                    {
+                        return true;
+                    }
+
+                    index[i]++;
+                    i = 1;
+                }
+                else
+                {
+                    index[i++] = 0;
+                }
+            }
+            return false;
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
---
+++ Chapter23-ProblemSolving/Permutations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PermutationUsingHeap permutation = new PermutationUsingHeap();
+            int[] values = new int[] { 0, 1, 2, 4 };
+            PermutationUsingHeap.ForAllPermutation(values, (vals) =>
+            {
+                Console.WriteLine(String.Join("", vals));
+                return false;
+            });
+        }
+    }
+}
